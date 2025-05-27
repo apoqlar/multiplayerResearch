b5aefd98b541568eb874e2276d514e432c806d03
YOG-SOTHOTH\Destruktor <furmaniak.radek@gmail.com>
2025-05-27 15:23:23 +0200

    okay networking grab by taking ownership before. not ideal

--- VRTemplate/Assets/Scripts/NetworkedGrab.cs
+++ VRTemplate/Assets/Scripts/NetworkedGrab.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class NetworkedGrab : MonoBehaviour
+public class NetworkedGrab : NetworkBehaviour
 {
     [SerializeField]
     private XRGrabInteractable grabInteractable;
@@ -15,15 +15,21 @@ public class NetworkedGrab : MonoBehaviour
     [SerializeField]
     private PredictedOwner predictedOwner;
 
+    [SerializeField]
+    private Rigidbody networkedBody;
+
     private void Start()
     {
         Debug.Log("Network grab started");
+        predictedOwner.TakeOwnership(true);
         grabInteractable.selectEntered.AddListener(OnSelectEntered);
+        grabInteractable.selectExited.AddListener(OnSelectExit);
     }
 
     private void OnDestroy()
     {
         grabInteractable.selectEntered.RemoveListener(OnSelectEntered);
+        grabInteractable.selectExited.RemoveListener(OnSelectExit);
     }
 
     private void OnSelectEntered(SelectEnterEventArgs args)
@@ -31,7 +37,19 @@ public class NetworkedGrab : MonoBehaviour
         Debug.Log($"Was owner: {networkObject.IsOwner}");
         Debug.Log("Getting ownership");
         predictedOwner.TakeOwnership(true);
+        SyncProperties(networkedBody.isKinematic, networkedBody.useGravity);
         Debug.Log($"Is owner: {networkObject.IsOwner}");
+    }
 
+    private void OnSelectExit(SelectExitEventArgs args)
+    {
+        SyncProperties(networkedBody.isKinematic, networkedBody.useGravity);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SyncProperties(bool isKinematic, bool useGravity)
+    {
+        networkedBody.isKinematic = isKinematic;
+        networkedBody.useGravity = useGravity;
     }
 }
